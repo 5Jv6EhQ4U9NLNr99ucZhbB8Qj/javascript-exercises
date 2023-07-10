f461ed3dd5ec56ca011825151b0aa2db1bf6023f
5Jv6EhQ4U9NLNr99ucZhbB8Qj <138229310+5Jv6EhQ4U9NLNr99ucZhbB8Qj@users.noreply.github.com>
2023-07-10 15:18:14 -0400

    Added code for sumAll() Added description of function and function processes. Added code to complete function. Enabled all tests in sumAll.spec.js.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+/*
+Function takes two integers. The numbers are ordered from least to
+greatests. Integers between two numbers are found by incrementing 
+positively by one in a for-loop. During each iteration, the
+current number is added to a variable.
+*/
 
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 | 
+        num2 < 0 | 
+        typeof num1 !== 'number' | 
+        typeof num2 !== 'number') {
+        return "ERROR";
+    } else if (num1 < num2) {
+        for (; num1 <= num2; num1++) {
+            sum += num1;
+        }
+    } else if (num2 < num1) {
+        for (; num2 <= num1; num2++) {
+            sum += num2;
+        }
+    } return sum;
 };
 
 // Do not edit below this line
