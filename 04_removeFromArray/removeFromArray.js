592b2e7445c7f5652e586fd04c5a129ababc7f6f
5Jv6EhQ4U9NLNr99ucZhbB8Qj <138229310+5Jv6EhQ4U9NLNr99ucZhbB8Qj@users.noreply.github.com>
2023-07-10 14:37:27 -0400

    Updated function removeFromArray() Added code to complete removeFromArray. Added comments to describe function process. Enabled all tests in removeFromArray.spec.js.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+/*
+Compares each item in source to target values. Each source item is
+compared to every target value and is assigned a true/false stored in
+the variable differenceCheck. If the source and target are different,
+differenceCheck will be true, and vice verss. Once the source item has
+been compared to each target value and if differenceCheck is true, the
+source item will be appended to newArray. This process is done for each
+source item. The function will return newArray; it is the source array
+with target values excluded.
+*/
 
+const removeFromArray = function(source, target) {
+    const newArray = [];
+    let differenceCheck;
+    for (let i = 0; i < source.length; i++) {
+        for (let j = 1; j < arguments.length; j++)
+            if (source[i] !== arguments[j]) {
+                differenceCheck = true;
+            } else {
+                differenceCheck = false;
+                break;
+            }
+        if (differenceCheck) {
+            newArray.push(source[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
