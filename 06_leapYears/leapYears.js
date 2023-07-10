3fe09af0723bb610ce8e0427ffb6fd0c882bb43a
5Jv6EhQ4U9NLNr99ucZhbB8Qj <138229310+5Jv6EhQ4U9NLNr99ucZhbB8Qj@users.noreply.github.com>
2023-07-10 15:33:40 -0400

    Added code for function leapYears() Added code to complete function leapYears(). Added description to describe function processes. Enabled all tests in leapYears.spec.js.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+/*
+Function takes years (int) as input.
+Leaps years determined by divisibility by 4 and not divisible by 100.
+If year is divisible by 100, must also be divisible by 400.
+*/
 
+const leapYears = function(year) {
+    if (year%4 === 0 && year%100 !== 0) {
+        return true;
+    } else if (year%100 === 0 && year%400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
