f358e2384206c7a0e353f1bbbce4114323708241
5Jv6EhQ4U9NLNr99ucZhbB8Qj <138229310+5Jv6EhQ4U9NLNr99ucZhbB8Qj@users.noreply.github.com>
2023-09-09 19:47:05 -0400

    Completed exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(data) {
+    let titles = [];
+    for (const book in data) {
+        titles.push(data[book]['title']);
+    }
+    return titles;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(data) {
+    let ages = [];
+    const currentYear = 2023;
+    for (const person in data) {
+        const yearOfBirth = data[person]['yearOfBirth'];
+        const yearOfDeath = data[person]['yearOfDeath'];
+        if (yearOfDeath === undefined) {
+            ages.push(currentYear - yearOfBirth);   
+        } else {
+            ages.push(yearOfDeath - yearOfBirth);   
+        }
+    }
 
+    let oldest = 0;
+    ages.forEach((age) => {
+        if (age > oldest) {
+            oldest = age;
+        }
+    }, 0);
+
+    return data[ages.findIndex((index) => index === oldest)];
 };
 
 // Do not edit below this line
